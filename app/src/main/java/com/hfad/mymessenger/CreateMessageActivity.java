adbaeebfb24919fb40fb4e8f7aded6f9f6841371
SunJuliana <juliakulisheva@yandex.ru>
2022-06-24 14:02:16 +0700

    Initial MyMesseger

---
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,17 @@
+package com.hfad.mymessenger;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+public class CreateMessageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+    //Вызвать onSendMessage() при щелчке на кнопке
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
+    }
+}
