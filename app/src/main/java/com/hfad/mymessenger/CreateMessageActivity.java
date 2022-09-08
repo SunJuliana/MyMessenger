3ec32ec16b505a71001ce687e10b03cf23ac7fe3
JuliaKulisheva <juliakulisheva@yandex.ru>
2022-09-08 14:28:01 +0700

    Created message app to send message with out application

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -1,16 +1,23 @@
 package com.hfad.mymessenger;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 public class CreateMessageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
     }
+
+    //Вызвать onSendMessage() при щелчке на кнопке
     public void onSendMessage(View view) {
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
         startActivity(intent);
     }
 }
